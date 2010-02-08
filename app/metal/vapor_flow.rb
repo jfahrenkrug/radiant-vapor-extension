8bf8ebded8e13dcab254b962b99fa57368b5c394
Johannes Fahrenkrug <jfahrenkrug@gmail.com>
2010-02-08 11:08:25 +0100

    query string passthru support

--- app/metal/vapor_flow.rb
+++ app/metal/vapor_flow.rb
@@ -10,6 +10,7 @@ class VaporFlow
       if env["PATH_INFO"].blank?
         return send_to_radiant
       end
+
       full_url = 'http://' + env["HTTP_HOST"] +  env["PATH_INFO"]
       relative_url = env["PATH_INFO"].sub(/^\//,'') #clean off the first slash, like it is stored in the db
       db_escaped_key = ActiveRecord::Base.connection.adapter_name =~ /mysql/i ? '`key`' : 'key'
@@ -19,13 +20,13 @@ class VaporFlow
         @@use_regexp = (config_key && config_key['value'] == 'true') ? true : false
       end
       if @@use_regexp
-        catch_with_regexp(full_url, relative_url)
+        catch_with_regexp(full_url, relative_url, env['QUERY_STRING'])
       else
-        catch_without_regexp(full_url, relative_url)
+        catch_without_regexp(full_url, relative_url, env['QUERY_STRING'])
       end
     end
     
-    def catch_with_regexp(full_url, relative_url)
+    def catch_with_regexp(full_url, relative_url, query_string)
       FlowMeter.all.sort.reverse.each do |meter|
         key = meter[0]
         value = meter[1]
@@ -34,7 +35,7 @@ class VaporFlow
         if (match)
           status = value[1].to_i
           redirect_url = self.match_substitute(value[0], match)
-          return [status, {"Location" => CGI.unescape(local_or_external_path(redirect_url))}, [status.to_s]]
+          return [status, {"Location" => CGI.unescape(local_or_external_path(redirect_url)) + (!query_string.blank? ? "?#{query_string}" : '')}, [status.to_s]]
           break
         else
           result = self.send_to_radiant
@@ -43,7 +44,7 @@ class VaporFlow
       return result
     end
 
-    def catch_without_regexp(full_url, relative_url)
+    def catch_without_regexp(full_url, relative_url, query_string)
       full_url = full_url.sub(/\/$/, '') unless full_url == '/' # drop the trailing slash for lookup
       relative_url = relative_url.sub(/\/$/, '') unless relative_url == '/' # drop the trailing slash for lookup
       a_match = FlowMeter.all[full_url]
@@ -51,7 +52,7 @@ class VaporFlow
       unless a_match.nil?
         status = a_match[1].to_i
         redirect_url = a_match[0]
-        [status, {"Location" => CGI.unescape(local_or_external_path(redirect_url))}, [status.to_s]]
+        [status, {"Location" => CGI.unescape(local_or_external_path(redirect_url)) + (!query_string.blank? ? "?#{query_string}" : '')}, [status.to_s]]
       else
         self.send_to_radiant
       end
